fc6bf9760ef056d12ba92ac663a5dcdb3c6febd4
Ebo Arcadia <yiboli.coding@gmail.com>
2022-04-22 09:07:10 -0400

    add login feature, using mutation type and define context with logic to obtain and decode email and find users

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -6,9 +6,23 @@ const resolvers = require('./resolvers')
 const LaunchAPI = require("./datasources/launch");
 const UserAPI = require("./datasources/user");
 
+const isEmail = require('isemail');
+
 const store = createStore();
 
 const server = new ApolloServer({
+    context: async ( { req }) => {
+        const auth = req.headers && req.headers.authorization || '';
+        const email = Buffer.from(auth, 'base64').toString('ascii');
+
+        if (!isEmail.validate(email)) return { user: null };
+
+        // find a user by their email
+        const users = await store.users.findOrCreateUser( { where: { email }});
+        const user = users && users[0] || null;
+
+        return { user: { ...user.dataValues }};
+    },
     typeDefs,
     resolvers,
     dataSources: () => ({
--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -60,4 +60,14 @@ module.exports = {
             );
         },
     },
+
+    Mutation: {
+        login: async (_, { email }, { dataSources }) => {
+            const user = await dataSources.userAPI.findOrCreateUser({ email });
+            if (user) {
+                user.token = Buffer.from(email).toString('base64');
+                return user;
+            }
+        },
+    },
 }
