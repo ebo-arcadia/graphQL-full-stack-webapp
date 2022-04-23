1f615e20c9351da1e64ad19b5c512a88d0fbe7c0
Ebo Arcadia <yiboli.coding@gmail.com>
2022-04-23 12:04:16 -0400

    add files to deploy server to apollo studio; import apollo client modules to fetch data with queries

---
+++ start/client/src/index.tsx
@@ -0,0 +1,38 @@
+import { ApolloClient, gql, NormalizedCacheObject, ApolloProvider} from "@apollo/client";
+import { cache } from './cache';
+import React from 'react';
+import ReactDOM from "react-dom";
+import Pages from './pages';
+import injectStyles from './styles';
+
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({cache, uri: 'http://localhost:4000/graphql'});
+
+// ...ApolloClient instantiated here...
+
+injectStyles();
+
+// Pass the ApolloClient instance to the ApolloProvider component
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <Pages />
+    </ApolloProvider>,
+    document.getElementById('root')
+)
+
+// the following script is used to test query using vanilla javascript
+
+// client.query({
+//     query: gql`
+//         query TestQuery {
+//             launch(id: 56) {
+//                 id
+//                 mission {
+//                     name
+//                 }
+//             }
+//         }
+//     `
+// })
+// .then(result => console.log(result));
